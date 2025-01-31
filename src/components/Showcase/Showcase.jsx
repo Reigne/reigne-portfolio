dd69a9aa91e2a7d27f9d6f8e8ae47df32a4427ee
Reigne <monteronaelija@yahoo.com>
2025-02-01 05:35:21 +0800

    Enhance Showcase component with marquee animation for skills; update layout and styling

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { CLIENTS_LOGO, SKILLS } from "../../constants";
 import "./Showcase.css"; // Import the CSS file for animations
+import { motion } from "framer-motion";
 
 const Showcase = () => {
   return (
-    <div className="w-full flex justify-center bg-transparent font-poppins overflow-hidden z-50">
-      <div className="w-full max-w-screen-2xl px-4 md:px-8 py-6 flex justify-between items-center gap-12">
+    <div className="w-full flex justify-center bg-transparent font-poppins overflow-hidden z-50 ">
+      <div className="w-full max-w-screen-2xl px-4 md:px-8 py-6 flex justify-between items-center gap-12 border-b border-background/10">
         <div className="min-w-fit">
           <p className="font-semibold tracking-wider text-md">MY SKILLS</p>
         </div>
 
         <div className="marquee">
-          <div className="marquee-content">
+          {/* <div className="marquee-content">
             {SKILLS.concat(SKILLS).map((client, index) => (
               <img
                 key={index}
@@ -22,6 +23,45 @@ const Showcase = () => {
                 //   onDragStart={preventImageDrag}
               />
             ))}
+          </div>
+        </div> */}
+
+          <div className="flex">
+            <motion.div
+              initial={{ x: 0 }}
+              animate={{ x: "-100%" }}
+              transition={{ duration: 40, repeat: Infinity, ease: "linear" }}
+              className="flex flex-shrink-0"
+            >
+              {SKILLS.concat(SKILLS).map((client, index) => (
+                <img
+                  key={index}
+                  className="inline-block mr-28 mix-blend-luminosity opacity-60"
+                  src={client.imageUrl}
+                  alt={client.name}
+                  width={60}
+                  //   onDragStart={preventImageDrag}
+                />
+              ))}
+            </motion.div>
+
+            <motion.div
+              initial={{ x: 0 }}
+              animate={{ x: "-100%" }}
+              transition={{ duration: 40, repeat: Infinity, ease: "linear" }}
+              className="flex flex-shrink-0"
+            >
+              {SKILLS.concat(SKILLS).map((client, index) => (
+                <img
+                  key={index}
+                  className="inline-block mr-28 mix-blend-luminosity opacity-60"
+                  src={client.imageUrl}
+                  alt={client.name}
+                  width={60}
+                  //   onDragStart={preventImageDrag}
+                />
+              ))}
+            </motion.div>
           </div>
         </div>
       </div>
