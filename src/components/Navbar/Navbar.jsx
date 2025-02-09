072f289d914c890994194f6ff73bdcc7a2f80392
Reigne <monteronaelija@yahoo.com>
2025-02-09 12:32:22 +0800

    added link in logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,12 +7,14 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-center absolute top-0 left-0 bg-transparent text-white font-poppins overflow-hidden z-50">
       <div className="w-full max-w-screen-2xl px-4 md:px-8 py-6 flex justify-between items-center gap-4">
-        <h1 className="text-2xl font-bold flex items-center gap-3">
-          <span>
-            <img src="/images/logo.png" alt="logo" className="w-6 h-6" />
-          </span>
-          reigne.
-        </h1>
+        <Link to="/">
+          <h1 className="text-2xl font-bold flex items-center gap-3">
+            <span>
+              <img src="/images/logo.png" alt="logo" className="w-6 h-6" />
+            </span>
+            reigne.
+          </h1>
+        </Link>
 
         {/* Hamburger Menu Button */}
         <button
@@ -52,7 +54,10 @@ const Navbar = () => {
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex items-center gap-14">
-          <Link to="/" className="relative hover:text-primary transition duration-300 cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-primary after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300">
+          <Link
+            to="/"
+            className="relative hover:text-primary transition duration-300 cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-primary after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300"
+          >
             about
           </Link>
           <li className="relative hover:text-primary transition duration-300 cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-primary after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300">
