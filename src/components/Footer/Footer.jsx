9c8e177e00cbaed53c2e4ad1b49b1d4469a57b02
Reigne <monteronaelija@yahoo.com>
2025-02-11 21:04:18 +0800

    update Footer component with navigation links and improved contact information; enhance social media section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import { FaFacebookF } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { RiInstagramFill } from "react-icons/ri";
 import { IoLogoGithub } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -12,7 +13,7 @@ const Footer = () => {
       className="bg-background font-poppins flex flex-col justify-center items-center px-4 bg-bottom text-white"
       //   style={{ backgroundImage: "url('/images/old/background-1.png')" }}
     >
-      <div className="w-full max-w-screen-2xl px-4 md:px-8 overflow-hidden grid grid-cols-1 md:grid-cols-4 gap-8 py-12 md:py-20">
+      <div className="w-full max-w-screen-2xl px-0 md:px-8 overflow-hidden grid grid-cols-1 md:grid-cols-4 gap-8 py-12 md:py-20">
         <div className="flex flex-col gap-3 md:gap-4">
           <h1 className="text-xl md:text-2xl font-bold flex items-center gap-3">
             <span>
@@ -30,29 +31,53 @@ const Footer = () => {
         <div className="flex flex-col gap-3 md:gap-4">
           <p className="text-base md:text-lg font-semibold">Pages</p>
 
-          <p className="text-zinc-400 text-sm md:text-base">Home</p>
-          <p className="text-zinc-400 text-sm md:text-base">Contact</p>
-          <p className="text-zinc-400 text-sm md:text-base">404</p>
+          <Link to="/">
+            <p className="text-zinc-400 text-sm md:text-base">Home</p>
+          </Link>
+
+          <Link to="/contact">
+            <p className="text-zinc-400 text-sm md:text-base">Contact</p>
+          </Link>
+
+          <Link to="404">
+            <p className="text-zinc-400 text-sm md:text-base">404</p>
+          </Link>
         </div>
 
         <div className="flex flex-col gap-3 md:gap-4">
           <p className="text-base md:text-lg font-semibold">Contact Me</p>
 
-          <p className="text-zinc-400 text-sm md:text-base">elijareigne@gmail.com</p>
-          <p className="text-zinc-400 text-sm md:text-base">(63+) 921 9201 772</p>
           <p className="text-zinc-400 text-sm md:text-base">
-            1234 St. California, USA Cityvillem State, 1234
+            elijareigne@gmail.com
+          </p>
+          <p className="text-zinc-400 text-sm md:text-base">
+            (63+) 921 9201 772
+          </p>
+          <p className="text-zinc-400 text-sm md:text-base">
+            Santa Rosa City, Dita, Laguna, Philippines
           </p>
         </div>
 
         <div className="flex flex-col gap-3 md:gap-4">
           <p className="text-base md:text-lg font-semibold">Follow Me</p>
+          <p className="text-zinc-400 text-sm md:text-base">
+            Stay connected with me
+          </p>
 
-          <div className="flex gap-4 justify-start md:justify-center">
-            <FaFacebookF className="text-xl text-white" />
-            <RiInstagramFill className="text-xl text-white" />
-            <FaTwitter className="text-xl text-white" />
-            <IoLogoGithub className="text-xl text-white" />
+          <div className="flex gap-4 justify-start">
+            <a className="cursor-pointer">
+              <FaFacebookF className="text-xl text-white" />
+            </a>
+            <a className="cursor-pointer">
+              <RiInstagramFill className="text-xl text-white" />
+            </a>
+            <a className="cursor-pointer">
+              <FaTwitter className="text-xl text-white" />
+            </a>
+
+            <a className="cursor-pointer">
+              <IoLogoGithub className="text-xl text-white" />
+            </a>
           </div>
         </div>
       </div>
