c9e54f4198ba1eff99f669d246a4799943fc0276
Reigne <monteronaelija@yahoo.com>
2025-02-20 20:20:44 +0800

    add Toaster component for enhanced notification support

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import { HeroUIProvider } from "@heroui/react";
 import App from "./App";
 import "./index.css";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <HeroUIProvider>
+        <Toaster />
         <App />
       </HeroUIProvider>
     </BrowserRouter>
