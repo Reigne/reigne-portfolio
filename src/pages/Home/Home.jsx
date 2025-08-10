f56daaa6f47e150d382d991b3f57c788d16b09c2
Reigne <monteronaelija@yahoo.com>
2025-08-10 15:06:24 +0800

    feat: wrap main content in ClickSpark component for enhanced interactive click animations on Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,18 +8,27 @@ import Services from "../../components/Services/Services";
 import Footer from "../../components/Footer/Footer";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Metadata from "../../components/Layout/Metadata";
+import ClickSpark from "../../components/Animations/ClickSpark";
 
 const Home = () => {
   return (
     <>
       <Metadata title="Home" />
       <Navbar />
-      <Hero />
-      <Showcase />
-      <AboutMe />
-      <Services />
-      <Projects />
-      <Footer />
+      <ClickSpark
+        sparkColor="#84cc16"
+        sparkSize={12}
+        sparkRadius={20}
+        sparkCount={10}
+        duration={500}
+      >
+        <Hero />
+        <Showcase />
+        <AboutMe />
+        <Services />
+        <Projects />
+        <Footer />
+      </ClickSpark>
     </>
   );
 };
